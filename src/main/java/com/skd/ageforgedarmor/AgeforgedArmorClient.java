8ae9f474eb0401a078f42b63fd4f36991ebec08f
LlaGuiTo <santiago.losada.borrajo@gmail.com>
2026-07-12 14:27:42 +0200

    Initial commit

---
+++ src/main/java/com/skd/ageforgedarmor/AgeforgedArmorClient.java
@@ -0,0 +1,31 @@
+package com.skd.ageforgedarmor;
+
+import net.minecraft.client.Minecraft;
+import net.neoforged.api.distmarker.Dist;
+import net.neoforged.bus.api.SubscribeEvent;
+import net.neoforged.fml.ModContainer;
+import net.neoforged.fml.common.EventBusSubscriber;
+import net.neoforged.fml.common.Mod;
+import net.neoforged.fml.event.lifecycle.FMLClientSetupEvent;
+import net.neoforged.neoforge.client.gui.ConfigurationScreen;
+import net.neoforged.neoforge.client.gui.IConfigScreenFactory;
+
+// This class will not load on dedicated servers. Accessing client side code from here is safe.
+@Mod(value = AgeforgedArmor.MODID, dist = Dist.CLIENT)
+// You can use EventBusSubscriber to automatically register all static methods in the class annotated with @SubscribeEvent
+@EventBusSubscriber(modid = AgeforgedArmor.MODID, value = Dist.CLIENT)
+public class AgeforgedArmorClient {
+    public AgeforgedArmorClient(ModContainer container) {
+        // Allows NeoForge to create a config screen for this mod's configs.
+        // The config screen is accessed by going to the Mods screen > clicking on your mod > clicking on config.
+        // Do not forget to add translations for your config options to the en_us.json file.
+        container.registerExtensionPoint(IConfigScreenFactory.class, ConfigurationScreen::new);
+    }
+
+    @SubscribeEvent
+    static void onClientSetup(FMLClientSetupEvent event) {
+        // Some client setup code
+        AgeforgedArmor.LOGGER.info("HELLO FROM CLIENT SETUP");
+        AgeforgedArmor.LOGGER.info("MINECRAFT NAME >> {}", Minecraft.getInstance().getUser().getName());
+    }
+}
